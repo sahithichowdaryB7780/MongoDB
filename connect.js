d60773fc75e8595dfd682461bca4a0dc5952e83b
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-21 02:29:17 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/demo_db');
+    await mongoose.connect('mongodb://127.0.0.1:27017/demo_db');
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Failed to connect to MongoDB:', error.message);
