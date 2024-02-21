5ca6fdb746320334df5e64f8c4a3c37fdeb17a16
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-21 21:38:11 +0530

    Delete item.js

--- item.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose');
-
-const itemSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-});
-
-module.exports = mongoose.model('names', itemSchema);
