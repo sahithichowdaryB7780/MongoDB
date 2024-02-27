ddb6607b61f8c81e97257b952b6938d2307f7c1e
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-27 13:42:44 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -30,45 +30,45 @@ describe('Test Suite', () => {
 
   describe('create function', () => {
     it('should create a document in the database', async () => {
-      const result = await create('Type-D', '15W', 'ABC Electronics');
-      expect(result).to.equal(1);
+      const createDone = await create('Type-D', '15W', 'ABC Electronics');
+      expect(createDone).to.equal(1);
     });
   });
 
 
   describe('read function', () => {
     it('should return 1 if document with given type exists', async () => {
-      const result = await read('Type-A');
-      expect(result).to.equal(1);
+      const readDone = await read('Type-A');
+      expect(readDone).to.equal(1);
     });
 
     it('should return 0 if document with given type does not exist', async () => {
-      const result = await read('Micro-HDMI');
-      expect(result).to.equal(0);
+      const readDone = await read('Micro-HDMI');
+      expect(readDone).to.equal(0);
     });
   });
 
   describe('update function', () => {
     it('should update the manufacturer of existing document', async () => {
-      const result = await update('Type-A', '100W');
-      expect(result).to.equal(1);
+      const updateDone = await update('Type-A', 'A Electronics');
+      expect(updateDone).to.equal(1);
     });
 
     it('should return 0 if document with given type does not exist', async () => {
-      const result = await update('Micro-HDMI', '7.5W');
-      expect(result).to.equal(0);
+      const updateDone = await update('Micro-HDMI', 'ABC Electronics');
+      expect(updateDone).to.equal(0);
     });
   });
 
   describe('remove function', () => {
     it('should remove the document with given type', async () => {
-      const result = await remove('Type-A');
-      expect(result).to.equal(1);
+      const removeDone = await remove('Type-A');
+      expect(removeDone).to.equal(1);
     });
 
     it('should return 0 if document with given type does not exist', async () => {
-      const result = await remove('Micro-HDMI');
-      expect(result).to.equal(0);
+      const removeDone = await remove('Micro-HDMI');
+      expect(removeDone).to.equal(0);
     });
   });
 });
